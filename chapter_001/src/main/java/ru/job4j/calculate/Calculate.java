eb94baf127ac1c285182c0a249fc9f5d2256b47a
Nikolay.Gorbunov <krot19831983@mail.ru>
2018-07-24 23:02:40 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+ *Class Calculate решение задачи 1.3 урок 1
+ *@author nikolay gorbunov
+ *@since 0.0.1
+*/
 public class Calculate {
+	/**
+	*Конструктор, вывод строки Hello world в консоль
+	*@param arg - arg.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Nikolay Gorbunov (mailto:krot19831983@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
