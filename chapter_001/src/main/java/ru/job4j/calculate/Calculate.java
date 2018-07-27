2bc10e803c7a09222005026ad65293bef51e6291
Nikolay <krot19831983@mail.ru>
2018-07-27 12:46:37 +0300

    1.3.1 Maven Plugins [#49546]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,7 +8,7 @@ package ru.job4j.calculate;
 public class Calculate {
 	/**
 	*Конструктор, вывод строки Hello world в консоль
-	*@param arg - arg.
+	*@param args - args.
 	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
