ee2b920f701e4a86a34f4ed3b3bdf13af8abfb2c
Nikolay <krot19831983@mail.ru>
2018-07-29 23:58:17 +0300

    3.4. Расстояние между точками в системе координат [#188]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -9,20 +9,38 @@ public class Point {
 	 private int x;
 	 private int y;
 
+    /**
+     * Метод для задания координат.
+     * @param x Координта по оси X.
+     * @param y Координта по оси Y.
+     */
 	 public Point(int x, int y) {
 		  this.x = x;
 		  this.y = y;
 	 }
-	 
+
+    /**
+     * Расчёт расстояния между точками.
+     * @param that удалнная точка
+     * @return расстояние между точками.
+     */
 	public double distanceTo(Point that) {
 		return Math.sqrt(
 				Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
 		);
 	}
 
+	/**
+	 *Расчёт расстояния между точками,
+	 *задание координат
+	 *вывод координат в консоль
+	 *@param args - args.
+	 */
+
 	public static void main(String[] args) {
+	//задание координат через метод Point
 	Point a = new Point(0, 1);
-	Point b = new Point(2, 5);
+	Point b = new Point(0, 10);
 	// сделаем вызов метода
 	System.out.println("x1 = " + a.x);
 	System.out.println("y1 = " + a.y);
