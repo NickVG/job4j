3f1821edd55a724ac8b7093bdc57632a563e5ed2
Nikolay Gorbunov <mailto:krot19831983@mail.ru>
2018-08-20 17:14:10 +0300

    Tracker

---
+++ chapter_002/src/main/java/ru/job4j/Tracker/StartUI.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+/**
+ * Константа меню для добавления новой заявки.
+ */
+private static final String ADD = "0";
+/**
+ * Константа для Показа всех заявок.
+ */
+private static final String SHOW = "1";
+/**
+ * Константа для редактирования заявки.
+ */
+private static final String EDIT = "2";
+/**
+ * Константа для удаления заявки
+ */
+private static final String DELETE = "3";
+/**
+ * Константа для поиска заявки по ID.
+ */
+private static final String FINDBYID = "4";
+/**
+ * Константа для поиска заявки по имени.
+ */
+private static final String FINDBYNAME = "5";
+/**
+ * Константа для выхода из цикла.
+ */
+private static final String EXIT = "6";
+
+/**
+ * Класс StartUI точка входа в программу
+ */
+public class StartUI {
+    public static void main(String[] args){
+        boolean exit = false;
+        while(exit) {
+            if (...) {
+            } else if (...) {
+            }
+        }
+    }
+
+}
--- chapter_002/src/main/java/ru/job4j/Tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/Tracker/Tracker.java
@@ -1,58 +1,59 @@
-package ru.job4j.Tracker;
+package ru.job4j.tracker;
 
 public class Tracker {
-    private Item[] items = new Item[100];
-
     /**
-     * Метод для добавление заявок
-     * @param item
-     * @return
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+    /**
+     * Метод реализующий добавление заявки в хранилище
+     * @param item новая заявка
      */
     public Item add(Item item){
-
+        this.position++;
+        item.setId(this.generateId());
+        return this.items[(position - 1)];
     }
-
     /**
      * Метод для редактирования заявок
-     * @param id
-     * @param item
+     * @param id    ID заявки
+     * @param item  сама новая заявка
      */
     public void replace(String id, Item item){
 
     }
-
     /**
      * Метод для удаления заявок
      * @param id
      */
     public void delete(String id){
-
     }
-
     /**
      * Метод для получения списка всех заявок
      * @return
      */
     public Item[] findAll(){
-
+        return item;
     }
-
     /**
      * Метод для получения списка по имени
      * @param key
      * @return
      */
     public Item[] findByName(String key){
-
+        return item;
     }
-
     /**
      * Метод для получения заявки по id
-     * @param id
+     * @param id ID заявки
      * @return
      */
     public Item findById(String id){
-
+        return item;
     }
 
 
