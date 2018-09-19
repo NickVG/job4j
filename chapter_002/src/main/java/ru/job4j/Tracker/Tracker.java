66910bcc0f2ea180db53c34bb4b411c63c0ad255
Nikolay Gorbunov <mailto:krot19831983@mail.ru>
2018-09-19 10:46:31 +0300

    Tracker

--- chapter_002/src/main/java/ru/job4j/Tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/Tracker/Tracker.java
@@ -1,4 +1,6 @@
 package ru.job4j.tracker;
+import java.util.Random;
+import java.util.*;
 
 public class Tracker {
     /**
@@ -9,6 +11,25 @@ public class Tracker {
      * Указатель ячейки для новой заявки.
      */
     private int position = 0;
+    private static final Random RN = new Random();
+
+    /**
+     * метод генерирующий ID заявки
+     * @return ID заявки
+     */
+    String generateId() {
+        return String.valueOf(System.currentTimeMillis() + RN.nextInt());
+    }
+
+    int idQuantity () {
+        int quantity = 0;
+        for (int i = 0; i < 100; i++) {
+            if (items[i] != null) {
+                quantity++;
+            }
+        }
+        return quantity;
+    }
     /**
      * Метод реализующий добавление заявки в хранилище
      * @param item новая заявка
@@ -24,7 +45,7 @@ public class Tracker {
      * @param item  сама новая заявка
      */
     public void replace(String id, Item item){
-
+        findById(id) = item;
     }
     /**
      * Метод для удаления заявок
@@ -37,23 +58,43 @@ public class Tracker {
      * @return
      */
     public Item[] findAll(){
-        return item;
+        Item[] result = new Item[idQuantity()];
+        int j = 0;
+        for (int i = 0; i < 100; i++) {
+            if (this.items[i] != null) {
+                result[j++] = this.items[i];
+            }
+        }
+        return result;
     }
     /**
      * Метод для получения списка по имени
      * @param key
      * @return
      */
-    public Item[] findByName(String key){
-        return item;
+    public Item[] findByName(String key) {
+            Item result [] = null;
+            for (Item item : items) {
+                if (item !=null && item.getId().equals(key)) {
+                }
+            }
+            return result;
     }
     /**
      * Метод для получения заявки по id
      * @param id ID заявки
-     * @return
+     * @return result заявка
      */
-    public Item findById(String id){
-        return item;
+    public Item findById(String id)
+    {
+        Item result = null;
+        for (Item item : items) {
+            if (item !=null && item.getId().equals(id)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
     }
 
 
